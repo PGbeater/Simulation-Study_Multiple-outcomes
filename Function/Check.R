4dbe3506365eea4edf249ad5519d09e3fa2cc26d
Junjie Wu <118987228+PGbeater@users.noreply.github.com>
2025-02-14 18:44:11 +0100

    First commit

---
+++ Function/Check.R
@@ -0,0 +1,6 @@
+# check the distribution
+hist(Association_and_PSmodel_S$check_X_form$`Related to how many Outcomes`,10)
+hist(Association_and_PSmodel_SC$check_X_form$`Related to how many Outcomes`,10)
+hist(Association_and_PSmodel_C$check_X_form$`Related to how many Outcomes`,10)
+hist(Association_and_PSmodel_US$check_X_form$`Related to how many Outcomes`,10)
+hist(Association_and_PSmodel_B$check_X_form$`Related to how many Outcomes`,10)
